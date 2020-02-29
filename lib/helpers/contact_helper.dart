56ef7a5891a2bc6d34c7cdce94fb200b41f1b86c
ParzivalOne <mateusdelg@gmail.com>
2020-02-29 18:25:49 -0300

    database working

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -18,7 +18,7 @@ class ContactHelper {
 
   Database _db;
   Future<Database> get db async {
-    if(db != null){
+    if(_db != null){
       return _db;
     }else{
       _db = await initDb();
@@ -27,7 +27,7 @@ class ContactHelper {
   }
   Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "contacts.db");
+    final path = join(databasesPath, "contactsnew.db");
     
     return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
       await db.execute(
@@ -38,7 +38,7 @@ class ContactHelper {
 
   Future<Contact> saveContact(Contact contact) async {
     Database dbContact = await db;
-    contact.id = (await dbContact.insert(contactTable, contact.toMap())) as String;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
     return  contact;
   }
 
@@ -54,7 +54,36 @@ class ContactHelper {
     }else{
       return null;
     }
+  }
 
+  Future<int> deleteContact(int id) async{
+    Database dbContact = await db;
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?", whereArgs: [contact.id]);
+  }
+
+  Future<List> getAllContacts() async {
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContact = List();
+    for (Map m in listMap){
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+
+  Future<int> getNumber() async{
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+  Future close() async{
+    Database dbContact = await db;
+    dbContact.close();
   }
 
 }
@@ -65,11 +94,13 @@ class ContactHelper {
 //   0      daniel  xxx      466464     img/aa/aaa/dwa
 
 class Contact {
-  String id;
+  int id;
   String name;
   String email;
   String phone;
   String img;
+
+  Contact();
 
   Contact.fromMap(Map map){
     id = map[idColumn];
