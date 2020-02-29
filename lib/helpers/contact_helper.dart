cc3afcbdb4bc2cf5b1adbb5b2a59ad41ba100775
ParzivalOne <mateusdelg@gmail.com>
2020-02-29 17:38:19 -0300

    database structure

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,5 +1,8 @@
 import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -7,8 +10,56 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+  Future<Database> get db async {
+    if(db != null){
+      return _db;
+    }else{
+      _db = await initDb();
+      return _db;
+    }
+  }
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+    
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+      );
+    });
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = (await dbContact.insert(contactTable, contact.toMap())) as String;
+    return  contact;
+  }
+
+  Future<Contact> getContact(int id) async{
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
+
+    if (maps.length > 0){
+      return Contact.fromMap(maps.first);
+    }else{
+      return null;
+    }
+
+  }
 
 }
+
+
 
 //  id     name    email     phone     img
 //   0      daniel  xxx      466464     img/aa/aaa/dwa
